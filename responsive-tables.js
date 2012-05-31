0f815224989ddcd5949de54e64c634e09e5930ac
myfreeweb <floatboth@me.com>
2012-05-31 22:42:22 +0400

    unfuck the js

--- responsive-tables.js
+++ responsive-tables.js
@@ -1,42 +1,36 @@
 $(document).ready(function() {
   var switched = false;
-  var updateTables = function() {
-    if (($(window).width() < 767) && !switched ){
-      switched = true;
-      $("table.responsive").each(function(i, element) {
-        splitTable($(element));
-      });
-      return true;
-    }
-    else if (switched && ($(window).width() > 767)) {
-      switched = false;
-      $("table.responsive").each(function(i, element) {
-        unsplitTable($(element));
-      });
-    }
-  };
-   
-  $(window).load(updateTables);
-  $(window).bind("resize", updateTables);
-   
-	
-	function splitTable(original)
-	{
-		original.wrap("<div class='table-wrapper' />");
-		
-		var copy = original.clone();
-		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-		copy.removeClass("responsive");
-		
-		original.closest(".table-wrapper").append(copy);
-		copy.wrap("<div class='pinned' />");
-		original.wrap("<div class='scrollable' />");
-	}
-	
-	function unsplitTable(original) {
-    original.closest(".table-wrapper").find(".pinned").remove();
-    original.unwrap();
-    original.unwrap();
-	}
 
+  function updateTables() {
+    var fixTable;
+    if (($(window).width() < 767) && !switched) {
+      switched = true;
+      fixTable = splitTable;
+    } else if (switched && ($(window).width() > 767)) {
+      switched = false;
+      fixTable = unsplitTable;
+    }
+    $('table.responsive').each(function(i, element) {
+      fixTable($(element));
+    });
+  };
+
+  $(window).load(updateTables).bind('resize', updateTables);
+
+  function splitTable(original) {
+    original.wrap('<div class=table-wrapper>');
+
+    var copy = original.clone();
+    copy.removeClass('responsive').find('td:not(:first-child), th:not(:first-child)').css('display', 'none');
+
+    original.closest('.table-wrapper').append(copy);
+    copy.wrap('<div class=pinned>');
+    original.wrap('<div class=scrollable>');
+  }
+
+  function unsplitTable(original) {
+    original.closest('.table-wrapper').find('.pinned').remove();
+    original.unwrap();
+    original.unwrap();
+  }
 });
