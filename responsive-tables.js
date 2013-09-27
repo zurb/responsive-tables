7a2bffbff315a4d457104d50e1ea2276ddaf0ffd
Mark Hayes <mark@zurb.com>
2013-09-26 19:47:34 -0700

    update to ensure it works great with foundation 4.3.2

--- responsive-tables.js
+++ responsive-tables.js
@@ -1,67 +1,17 @@
-$(document).ready(function() {
-  var switched = false;
-  var updateTables = function() {
-    if (($(window).width() < 767) && !switched ){
-      switched = true;
-      $("table.responsive").each(function(i, element) {
-        splitTable($(element));
-      });
-      return true;
-    }
-    else if (switched && ($(window).width() > 767)) {
-      switched = false;
-      $("table.responsive").each(function(i, element) {
-        unsplitTable($(element));
-      });
-    }
-  };
-   
-  $(window).load(updateTables);
-  $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
-  $(window).on("resize", updateTables);
-   
-	
-	function splitTable(original)
-	{
-		original.wrap("<div class='table-wrapper' />");
-		
-		var copy = original.clone();
-		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-		copy.removeClass("responsive");
-		
-		original.closest(".table-wrapper").append(copy);
-		copy.wrap("<div class='pinned' />");
-		original.wrap("<div class='scrollable' />");
+(function($){
+  $("table.responsive").each(function(){
+    var table = $(this);
+    if (table.parent(".table-wrapper").length === 1) return;
 
-    setCellHeights(original, copy);
-	}
-	
-	function unsplitTable(original) {
-    original.closest(".table-wrapper").find(".pinned").remove();
-    original.unwrap();
-    original.unwrap();
-	}
-
-  function setCellHeights(original, copy) {
-    var tr = original.find('tr'),
-        tr_copy = copy.find('tr'),
-        heights = [];
-
-    tr.each(function (index) {
-      var self = $(this),
-          tx = self.find('th, td');
-
-      tx.each(function () {
-        var height = $(this).outerHeight(true);
-        heights[index] = heights[index] || 0;
-        if (height > heights[index]) heights[index] = height;
-      });
-
-    });
-
-    tr_copy.each(function (index) {
-      $(this).height(heights[index]);
-    });
-  }
-
-});
+    var wrapper = $("<div>").addClass("table-wrapper"),
+        pinned = $("<div>").addClass("pinned"),
+        scrollable = $("<div>").addClass("scrollable");
+    table.wrap(wrapper);
+    table.after(pinned);
+    table.wrap(scrollable);
+    pinned.html(table.clone().removeClass("responsive"));
+    
+    // Make everything but first column visible
+    pinned.find("th,td").not(pinned.find("th:first-child, td:first-child")).hide();
+  });
+})(jQuery);
