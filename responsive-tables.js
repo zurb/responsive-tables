f557a732caaeab396fb90d5834eb28f5b3040cf4
J.C. Bartle <jcbartle@live.com>
2017-05-07 17:14:14 -0400

    Fix incompatibility with jQuery 3.x (tested with jquery-3.2.1).

--- responsive-tables.js
+++ responsive-tables.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
   var switched = false;
   var updateTables = function() {
     if (($(window).width() < 767) && !switched ){
@@ -16,31 +16,29 @@ $(document).ready(function() {
     }
   };
    
-  $(window).load(updateTables);
+  $(window).on('load', updateTables);
   $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
-  $(window).on("resize", updateTables);
-   
-	
-	function splitTable(original)
-	{
-		original.wrap("<div class='table-wrapper' />");
-		
-		var copy = original.clone();
-		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-		copy.removeClass("responsive");
-		
-		original.closest(".table-wrapper").append(copy);
-		copy.wrap("<div class='pinned' />");
-		original.wrap("<div class='scrollable' />");
+  $(window).on("resize", updateTables);   
+  
+  function splitTable(original) {
+    original.wrap("<div class='table-wrapper' />");
+    
+    var copy = original.clone();
+    copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
+    copy.removeClass("responsive");
+    
+    original.closest(".table-wrapper").append(copy);
+    copy.wrap("<div class='pinned' />");
+    original.wrap("<div class='scrollable' />");
 
     setCellHeights(original, copy);
-	}
-	
-	function unsplitTable(original) {
+  }
+  
+  function unsplitTable(original) {
     original.closest(".table-wrapper").find(".pinned").remove();
     original.unwrap();
     original.unwrap();
-	}
+  }
 
   function setCellHeights(original, copy) {
     var tr = original.find('tr'),
@@ -63,5 +61,4 @@ $(document).ready(function() {
       $(this).height(heights[index]);
     });
   }
-
 });
